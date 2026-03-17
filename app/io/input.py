d140133a0d8e0edf67a861eafba37d398818ab3e
Oleksandr Kopiievskyi <o.kopiievskyi@gmail.com>
2026-03-17 16:32:58 +0200

    Task 4.3

--- app/io/input.py
+++ app/io/input.py
@@ -1,1 +1,9 @@
-#task 2
+#1
+def input_text_from_console():
+    pass
+#2
+def read_file():
+    pass
+#3
+def read_file_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+#1
+def output_text_to_console():
+    pass
+#2
+def write_to_file():
+    pass
